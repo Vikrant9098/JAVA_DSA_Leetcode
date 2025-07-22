043786c386d60b70ae564950e25c80a72ea53fb4
Vikrant Naik <vikrantnaik4802@gmail.com>
2025-07-22 20:08:47 +0530

    Problem.No.215

---
+++ 215_KthLargestElementinanArray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) 
+    {
+        // Min-heap to keep track of top k largest elements
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for(int no : nums)
+        {
+            queue.add(no);           // Add current number to the heap
+
+            if(queue.size() > k)     // If heap size exceeds k
+            {
+                queue.remove();      // Remove the smallest element
+            }
+        }
+
+        return queue.poll();         // Top of min-heap is the kth largest element
+    }
+}
