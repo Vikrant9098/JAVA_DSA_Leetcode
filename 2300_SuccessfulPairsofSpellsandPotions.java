8aad6f66f7db5f896c0b0d042b6b795f9587627f
Vikrant Naik <vikrantnaik4802@gmail.com>
2025-07-24 19:44:06 +0530

    Problem.No.2300

--- 2300_SuccessfulPairsofSpellsandPotions.java
+++ 2300_SuccessfulPairsofSpellsandPotions.java
@@ -2,51 +2,53 @@ import java.util.Arrays;
 
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        // Sort the potions array to apply binary search
+        // Sort the potions array to enable binary search
         Arrays.sort(potions);
 
-        // Store the number of spells
+        // Get the number of spells
         int n = spells.length;
 
-        // Store the number of potions
+        // Get the number of potions
         int m = potions.length;
 
-        // Result array to hold the count of successful pairs for each spell
+        // Array to store the count of successful pairs for each spell
         int[] result = new int[n];
 
         // Iterate through each spell
         for (int i = 0; i < n; i++) {
-            int spell = spells[i;
+            // Current spell strength
+            int spell = spells[i];
 
             // Binary search bounds
             int low = 0;
             int high = m - 1;
 
-            // This will store the first index of a valid potion
+            // Variable to track the first valid index in potions
             int index = m;
 
-            // Perform binary search
+            // Binary search to find the leftmost potion that forms a successful pair
             while (low <= high) {
+                // Calculate mid index
                 int mid = low + (high - low) / 2;
 
-                // Multiply spell and potions[mid] and compare with success
+                // Calculate product and ensure no overflow using type casting
                 long product = (long) spell * potions[mid];
 
+                // If product is enough, try to find a smaller index
                 if (product >= success) {
-                    // Valid pair found, try to find earlier index
                     index = mid;
                     high = mid - 1;
                 } else {
-                    // Too small, search right half
+                    // If product is too small, search in right half
                     low = mid + 1;
                 }
             }
 
-            // The number of successful potions is from index to end => m - index
+            // Number of successful pairs = potions from index to end
             result[i] = m - index;
         }
 
-        // Return the result array
+        // Return final result array
         return result;
     }
 }
