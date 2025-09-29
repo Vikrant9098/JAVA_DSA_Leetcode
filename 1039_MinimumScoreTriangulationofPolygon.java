6ee6e4c9b894a56377cf65974404fc8130e5f389
Vikrant Naik <vikrantnaik4802@gmail.com>
2025-09-29 21:07:21 +0530

    Problem.No.1039

---
+++ 1039_MinimumScoreTriangulationofPolygon.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minScoreTriangulation(int[] values) {
+        int n = values.length;
+        int[][] dp = new int[n][n];
+
+        // gap represents the length of subpolygon
+        for (int len = 2; len < n; len++) {
+            for (int i = 0; i + len < n; i++) {
+                int j = i + len;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; k++) {
+                    int score = dp[i][k] + dp[k][j] + values[i] * values[j] * values[k];
+                    dp[i][j] = Math.min(dp[i][j], score);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+}
